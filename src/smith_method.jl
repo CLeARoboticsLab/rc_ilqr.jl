bccfe110af9aef3b38a9c0518720a31c04c0fb50
Ryan Park <ryanjpark03@gmail.com>
2024-06-05 16:47:26 -0500

    smith method comments, lqr comments

--- src/smith_method.jl
+++ src/smith_method.jl
@@ -2,38 +2,63 @@ using LinearAlgebra
 
 """
 Uses Smith's method to solve the Lyapunov Equation
+
+Notes: 
+    Lyapunov equation: Aₒᵀ * X + X * Aₒ = - D * Dᵀ
+
+Parameters:
+    Aₒ =  Term being transposed in the left-hand side of the Lyapunov equation
+    DDᵀ = Right hand side of the Lyapunov equation
+    tol = Tolerance threshold, once the new x values are tol close
+        to the old x values we stop iterating
+    iter_lim = Maximum number of iterations
+    q = A float less than 0 which may contributes to the speed of convergence
+
+Output:
+    x_new = A value of X such that the Lyapunov equation holds
+
 """
 
-function solve_lyapunov(Aₒ :: Matrix{Float64}, DDᵀ :: Matrix{Float64}, tol :: Float64, iter_lim :: Int64 = 50, q :: Float64 = -1.0)
+function solve_lyapunov(Aₒ :: Matrix{Float64}, DDᵀ :: Matrix{Float64},
+    tol :: Float64, iter_lim :: Int64 = 50, q :: Float64 = -1.0)
 
-    n = size(Aₒ)[1]
+    # Precondition check
+    if q >= 0
+        println("q must be a Float less than 0")
+    end
 
+    # Initial guess for X
     xₒ = zeros(size(Aₒ))
 
-    Q = q*I(n)
+    # Q is a shorthand for q * I
+    Q = q*I(size(Aₒ)[1])
 
     iter = 0
 
+    # Initialize iteration values
     x_new = xₒ
+    x_old = zeros(size(Aₒ))
 
     while iter < iter_lim
 
         x_old = x_new
 
+        # Placeholder terms for solving for X
         U = (Aₒ - Q) * inv(Aₒ + Q)
         V = -2 * q * inv(Aₒ' + Q) * DDᵀ * inv(Aₒ + Q)
 
+        # Analytical expression for iterating to a new X
         x_new = U' * x_old * U + V
 
+        # If tolerance threshold reached, stop
         if LinearAlgebra.norm(x_new - x_old , 2) < tol
-
-            println("Lyapunov Equation Converged")
+            # println("Lyapunov Equation Converged")
             return x_new
 
         end
     end
     println("ERROR: Lyapunov Equation failed to converge.")
-    return -1
+    return nothing
 end
 
 export solve_lyapunov
--- src/solve_lqr.jl
+++ src/solve_lqr.jl
@@ -5,15 +5,42 @@ using LinearAlgebra
 include("solve_riccati.jl")
 
 """
-OUTPUTS: 
+Solves the continuous lqr problem for the given A, B, Q, and R.
+Problem defined as:
+    minᵤ xᵀ * Q * x + uᵀ * R * u
+    such that
+    ẋ = A * x + B * u
+    u = K * x
 
+Notes:
+    It has been proven that solving the Hamilton-Jacobi-Bellman equation for optimal u
+    coincides with the optimal u in the corresponding LQR problem. Solving the HJB
+    equation can be reduced to solving the riccati equation.
+
+    HJB : for all x, 
+        0 = minᵤ xᵀ * Q * x + uᵀ * R * u + ∇ₓJ⋆ * (A * x + B * u)
+        J_star is the optimal cost-to-go function (value function). For an LQ
+        problem, J_star is known to take the form J⋆(x) = xᵀ * S * x for some S.
+        
+    Solving the Riccati equation gives S.
+
+    u⋆ = -1 * (inv(R) * Bᵀ * S) * x = -1 * K * x
+
+Parameters:
+A = A as defined in problem above
+B = B as defined in problem above
+Q = Q as defined in problem above
+R = R as defined in problem above
+
+Outputs: 
 K - Control Feedback Gain -> u(t) = -Kx
-
 S - Defines optimal cost-to-go function -> J* = x' * S * x
 """
+function solve_continuous_lqr(A :: Matrix{Float64}, B :: Matrix{Float64},
+    Q :: Matrix{Float64}, R :: Matrix{Float64})
 
-function solve_continuous_lqr(A,B,Q,R)
-    S = solve_algebraic_riccati(A,B,Q,R)
+    # Solving LQR problem boils down to solving the riccati equation
+    S = solve_algebraic_riccati(A, B, Q, R)
 
     K = -inv(R) * B' * S
 
