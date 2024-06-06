4e358412068cb5e7584e01204c8d53b8da45291f
Ryan Park <ryanjpark03@gmail.com>
2024-06-06 09:24:46 -0500

    finite horizon continuous working

--- src/solve_lqr.jl
+++ src/solve_lqr.jl
@@ -1,11 +1,12 @@
 #using NLsolve
 # using Symbolics
 using LinearAlgebra
+using DifferentialEquations
 
 include("solve_riccati.jl")
 
 """
-Solves the continuous lqr problem for the given A, B, Q, and R.
+Solves the continuous, infinite horizon lqr problem for the given A, B, Q, and R.
 Problem defined as:
     minᵤ xᵀ * Q * x + uᵀ * R * u
     such that
@@ -22,7 +23,7 @@ Notes:
         J_star is the optimal cost-to-go function (value function). For an LQ
         problem, J_star is known to take the form J⋆(x) = xᵀ * S * x for some S.
         
-    Solving the Riccati equation gives S.
+    Solving the algebraic Riccati equation (ARE) gives S.
 
     u⋆ = -1 * (inv(R) * Bᵀ * S) * x = -1 * K * x
 
@@ -36,7 +37,7 @@ Outputs:
 K - Control Feedback Gain -> u(t) = -Kx
 S - Defines optimal cost-to-go function -> J* = x' * S * x
 """
-function solve_continuous_lqr(A :: Matrix{Float64}, B :: Matrix{Float64},
+function solve_continuous_inf_lqr(A :: Matrix{Float64}, B :: Matrix{Float64},
     Q :: Matrix{Float64}, R :: Matrix{Float64})
 
     # Solving LQR problem boils down to solving the riccati equation
@@ -47,6 +48,58 @@ function solve_continuous_lqr(A :: Matrix{Float64}, B :: Matrix{Float64},
     return (K,S)
 end
 
+#TODO: fix comments
+"""
+Solves the continuous, finite horizon lqr problem for the given A, B, Q, and R.
+Problem defined as:
+    minᵤ xᵀ * Q * x + uᵀ * R * u
+    such that
+    ẋ = A * x + B * u
+    u = K * x
+
+Notes:
+    It has been proven that solving the Hamilton-Jacobi-Bellman equation for optimal u
+    coincides with the optimal u in the corresponding LQR problem. Solving the HJB
+    equation can be reduced to solving the riccati equation.
+
+    HJB : for all x, 
+        0 = minᵤ xᵀ * Q * x + uᵀ * R * u + ∇ₓJ⋆ * (A * x + B * u) + ∇ₜJ⋆
+        J_star is the optimal cost-to-go function (value function). For an LQ
+        problem, J_star is known to take the form J⋆(x) = xᵀ * S * x for some S.
+        
+    Solving the differential Riccati equation gives S. It is difficult (numerically)
+    to solve for S, so set S = P * Pᵀ, then re-write as:
+        -Ṗ(t) = AᵀP(t) - .5 * S(t) * B * R⁻¹ * Bᵀ * P(t) + .5 * Q * P⁻ᵀ,
+            P(t_f) = sqrt(Q_f), Q_f > 0, P(t) invertible
+
+    u⋆ = -1 * (inv(R) * Bᵀ * S) * x = -1 * K * x
+
+Parameters:
+    A = A as defined in problem above
+    B = B as defined in problem above
+    Q = Q as defined in problem above
+    R = R as defined in problem above
+
+Outputs: 
+    K - Control Feedback Gain -> u(t) = -Kx
+    S - Defines optimal cost-to-go function -> J* = x' * S * x
+"""
+function solve_continuous_finite_lqr(A :: Matrix{Float64}, B :: Matrix{Float64},
+    Q :: Matrix{Float64}, Q_f :: Matrix{Float64}, R :: Matrix{Float64}, t_0, t_f, step_size)
+
+    S = solve_differential_riccati(A, B, Q, Q_f, R, t_0, t_f, step_size)
+
+    K = Array{Matrix{Float64}}(undef, size(S)[1], 1)
+
+    for i = 1 : size(S)[1]
+        K[i] = -1 * inv(R) * B' * S[i]
+    end
+
+    return (K, S)
+end
+
+
+
 """
 """
 
@@ -56,27 +109,15 @@ end
 
 
 """
-x[1] = x[1]
-x[2] = x[2]
-x[3]
-x[4]
 """
-
-function test()
-    
-    # [
-    #     -10 * (x[1] * x[1] + x[2] * x[3]) + 2 * x[1] + 1,
-    #     -10 * (x[1] * x[2] + x[2] * x[4]) + 2 * x[2] + x[1],
-    #     -10 * (x[1] * x[3] + x[3] * x[4]) + x[1] + 2 * x[3],
-    #     -10 * (x[4] * x[4] + x[2] * x[3]) + x[2] + x[3] + 2 * x[4] + 1
-    # ]
+function test_solve_continuous_inf_lqr()
 
     A = [1.0 1.0; 0 1.0]
     B = [1.0 0.0;0.0 1.0]
     Q = [1.0 0.0;0.0 1.0]
     R = 0.1 * [1.0 0.0;0.0 1.0]
 
-    K,S = solve_continuous_lqr(A,B,Q,R)
+    K,S = solve_continuous_info_lqr(A,B,Q,R)
 
     println("K: ", K)
     println("S: ", S)
@@ -84,5 +125,18 @@ function test()
 end
     
     
+function test_solve_continuous_finite_lqr()
 
+    A = [1.0 1.0; 0 1.0]
+    B = [1.0 0.0;0.0 1.0]
+    Q = [1.0 0.0;0.0 1.0]
+    Q_f = copy(Q)
+    R = 0.1 * [1.0 0.0;0.0 1.0]
+
+    K,S = solve_continuous_finite_lqr(A,B,Q,Q_f,R, 0, 11, 1)
+
+    println("K: ", K)
+    println("S: ", S)
+
+end
 
--- src/solve_riccati.jl
+++ src/solve_riccati.jl
@@ -7,23 +7,23 @@ using LinearAlgebra
 include("smith_method.jl")
 
 """
-Solve the Algebraic Riccati equation using Newton-Kleinman with Smith's method
-to solve the Lyapunov equation
+    Solve the Algebraic Riccati equation using Newton-Kleinman with Smith's method
+    to solve the Lyapunov equation
 
-Parameters: 
+    Parameters: 
 
-A: State space matrix defining dynamics (ẋ = Ax)
-B: State space matrix defining input (ẋ = Ax + Bu)
-Q: Matrix defining scaling of state in cost function J (J = ∫ xᵀQx + uᵀRu dt)
-R: Matrix defining scaling of input in cost funtion J (J = ∫ xᵀQx + uᵀRu dt)
-riccati_tolerance: Convergence tolerance for the Newton-Kleinman algorithm
-lyapunov_tolerance: Convergence tolerance for Smith's method
-max_riccati_iters: Maximum allowable iterations of the Newton-Kleinman algorithm
-max_lyapunov_iters: Maximum allowable iterations of Smith's method
+    A: State space matrix defining dynamics (ẋ = Ax)
+    B: State space matrix defining input (ẋ = Ax + Bu)
+    Q: Matrix defining scaling of state in cost function J (J = ∫ xᵀQx + uᵀRu dt)
+    R: Matrix defining scaling of input in cost funtion J (J = ∫ xᵀQx + uᵀRu dt)
+    riccati_tolerance: Convergence tolerance for the Newton-Kleinman algorithm
+    lyapunov_tolerance: Convergence tolerance for Smith's method
+    max_riccati_iters: Maximum allowable iterations of the Newton-Kleinman algorithm
+    max_lyapunov_iters: Maximum allowable iterations of Smith's method
 
-Outputs:
+    Outputs:
 
-P_new: Matrix minimizing the cost-to-go function J* (J* = xᵀSx)
+    P_new: Matrix minimizing the cost-to-go function J* (J* = xᵀSx)
 """
 function solve_algebraic_riccati(A :: Matrix{Float64}, B :: Matrix{Float64},
     Q :: Matrix{Float64}, R :: Matrix{Float64}, riccati_tolerance :: Float64 = 10e-5,
@@ -56,22 +56,65 @@ function solve_algebraic_riccati(A :: Matrix{Float64}, B :: Matrix{Float64},
 end
 
 """
-Anonymous function to solve the Lyapunov Equation (Aₒx + Aᵀₒx = -DDᵀ) in this
-specific problem context (Continuous infinite horizon LQR)
+"""
+function solve_differential_riccati(A :: Matrix{Float64}, B :: Matrix{Float64},
+    Q :: Matrix{Float64}, Q_f :: Matrix{Float64}, R :: Matrix{Float64}, t_0, t_f, step_size)
 
-Parameters:
+    # Differential riccati equation
+    function dre(du, u, p, t)
+        if t == t_f
+            P_t = Q_f^(1/2)
+        else
+            P_t = u
+        end
+        Ṗ = -1 * A' * P_t + .5 * P_t * P_t' * B * inv(R) * B' * P_t - .5 * Q * inv(P_t')
+        du = Ṗ
+    end
 
-S: (A+BK), Hurwitz matrix with feedback state gain as control input (ẋ = Ax + B(-Kx))
-Q: Matrix defining scaling of state in cost function J (J = ∫ xᵀQx + uᵀRu dt)
-K: State Feedback Gain Matrix as described above
-R: Matrix defining scaling of input in cost funtion J (J = ∫ xᵀQx + uᵀRu dt)
-lyapunov_tolerance: Convergence tolerance for Smith's method
-max_lyapunov_iters: Maximum allowable iterations of Smith's method
+    u0 = Q_f^(1/2)
+    t_span = (t_f, t_0)
 
-Outputs:
+    diff_riccati = ODEProblem(dre, u0, t_span)
 
-Passes the output of solve_lyapunov, which is the matrix that minimizes the cost-to-go 
-function, J* = xᵀSx, given the inputs
+    sol = solve(diff_riccati, reltol = 1e-6, saveat = step_size)
+
+    num_steps = length(sol.t)
+    if num_steps - 1 != (t_f - t_0) / step_size
+        println("num steps found by solver not matching with num steps implied by parameters")
+        println("num steps implied: ", (t_f - t_0) / step_size, " length of sol: ", num_steps)
+        return
+    end
+
+    S = Array{Matrix{Float64}}(undef, num_steps, 1)
+
+    for (i, s) in enumerate(tuples(sol))
+        u, t = s
+        round.(u, digits = 10)
+        S[i] = u * u'
+    end
+
+    return S
+end
+
+
+
+"""
+    Anonymous function to solve the Lyapunov Equation (Aₒx + Aᵀₒx = -DDᵀ) in this
+    specific problem context (Continuous infinite horizon LQR)
+
+    Parameters:
+
+    S: (A+BK), Hurwitz matrix with feedback state gain as control input (ẋ = Ax + B(-Kx))
+    Q: Matrix defining scaling of state in cost function J (J = ∫ xᵀQx + uᵀRu dt)
+    K: State Feedback Gain Matrix as described above
+    R: Matrix defining scaling of input in cost funtion J (J = ∫ xᵀQx + uᵀRu dt)
+    lyapunov_tolerance: Convergence tolerance for Smith's method
+    max_lyapunov_iters: Maximum allowable iterations of Smith's method
+
+    Outputs:
+
+    Passes the output of solve_lyapunov, which is the matrix that minimizes the cost-to-go 
+    function, J* = xᵀSx, given the inputs
 """
 function solve_lyapunov_custom(S :: Matrix{Float64}, Q :: Matrix{Float64}, K :: Matrix{Float64},
     R :: Matrix{Float64}, lyapunov_tolerance :: Float64, max_lyapunov_iters :: Int64 = 50)
@@ -80,16 +123,16 @@ end
 
 
 """
-Implements Gershgorin's Theorem to calculate the needed value to ensure that the inputted
-matrix is Hurwitz (Re(λᵢ)<0 ∀ i)
+    Implements Gershgorin's Theorem to calculate the needed value to ensure that the inputted
+    matrix is Hurwitz (Re(λᵢ)<0 ∀ i)
 
-Parameters
+    Parameters
 
-A: Matrix to be pushed to be Hurwitz
+    A: Matrix to be pushed to be Hurwitz
 
-Outputs:
+    Outputs:
 
-gamma: parameter that makes A Hurwitz with A = A-gamma*I
+    gamma: parameter that makes A Hurwitz with A = A-gamma*I
 """
 function gerghgorin_gamma(A :: Matrix{Float64})
     gamma = 0
@@ -101,3 +144,4 @@ function gerghgorin_gamma(A :: Matrix{Float64})
 end
 
 export solve_algebraic_riccati
+export solve_differential_riccati
