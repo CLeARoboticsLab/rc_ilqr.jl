755118f9887fec8807daa6e955ba24034e7c9ea9
Ryan Park <ryanjpark03@gmail.com>
2024-06-07 16:20:39 -0500

    created v3 for ilqr, trying to solve using iterated lqr

--- src/solve_ilqr.jl
+++ src/solve_ilqr.jl
@@ -2,6 +2,8 @@
 using Symbolics
 using LinearAlgebra
 
+include("solve_lqr.jl")
+
 """ 
 - Cost function is sum(x' * Q * x + u' * R * u) for all time + x_f' * Q_f * x_f
 Notes:
@@ -113,4 +115,117 @@ function solve_ilqr(dynamics_function :: Function, cost_function :: Function, Q 
     println("u: ", nominal_control_sequence)
 end
 
-export solve_ilqr
+function solve_ilqr_v3(dynamics_function :: Function, cost_function :: Function, Q :: Matrix{Float64},
+    Q_T :: Matrix{Float64}, R :: Matrix{Float64}, x_T :: Vector, x_0 :: Vector{Float64},
+    T :: Int64,threshold :: Float64, iter_limit :: Int64 = 100)
+
+    # The reason for this version is to try and use our lqr solvers. Kushagra told me:
+    # δuₖ = - K * δxₖ 
+    # New trajectory = f( xₖ, uₖ + δuₖ )
+
+    state_space_degree = size(Q)[1]
+    action_space_degree = size(R)[1]
+
+    nominal_state_sequence = Array{Vector{Float64}}(undef,1,T)
+    nominal_control_sequence = Array{Vector{Float64}}(undef,1,T)
+    δu = Array{Vector{Float64}}(undef,1,T)
+
+    nominal_state_sequence[1] = x_0
+    nominal_control_sequence[1] = zeros(action_space_degree)
+    δu[1] = zeros(action_space_degree)
+
+    # Initialize δu, u (nominal control sequence), trajectory (nominal state sequence)
+    for k = 2:T
+        nominal_control_sequence[k] = zeros(action_space_degree)
+        δu[k] = zeros(action_space_degree)
+        nominal_state_sequence[k] = dynamics_function(
+            nominal_state_sequence[k - 1],
+            nominal_control_sequence[k - 1])
+    end
+
+    
+    x = Symbolics.variables(:x, 1 : state_space_degree)
+    u = Symbolics.variables(:u, 1 : action_space_degree)
+
+    f = dynamics_function(x, u)
+    # f = Symbolics.build_function(dynamics_function, x, u)
+    dynamics = eval(Symbolics.build_function(f, x, u)[1])
+
+    ∇ₓf = Symbolics.jacobian(f, x)
+    ∇ᵤf = Symbolics.jacobian(f, u)
+
+    function find_A_B(state, action)
+        A_call = eval(build_function(∇ₓf, state, action, expression=Val{false})[1])
+        B_call = eval(build_function(∇ᵤf, state, action, expression=Val{false})[1])
+
+        A = 1.0 * A_call(state, action)
+        B = 1.0 * B_call(state, action)
+
+        return (A, B)
+    end
+        
+    function solve_δx(δu)
+        x = Array{Vector{Float64}}(undef, 1, T)
+        x[1] = zeros(state_space_degree)
+        for k = 2 : T
+            (A, B) = find_A_B(nominal_state_sequence[k], nominal_control_sequence[k])
+            x[k] = A * x[k - 1] + B * δu[k]
+        end
+        return x
+    end
+    
+
+    δx = solve_δx(δu)
+
+    iter = 0
+    old_cost = cost_function(Q, R, Q_T, nominal_state_sequence, nominal_control_sequence, x_T, T)
+
+    while iter <= iter_limit && norm(δu, 2) < threshold
+
+        # Backward Pass
+        for k = T - 1 : -1 : 1
+            # TODO: Unsure
+            (A, B) = find_A_B(nominal_state_sequence[k], nominal_control_sequence[k])
+            K, S = solve_discrete_finite_lqr(A, B, Q, R, T - k)
+            δu[k] = - K[1] * δx[k]
+        end
+
+        for k = 2 : 1 : T
+            # TODO: Ends are weird
+            nominal_state_sequence[k] = dynamics_function(
+                nominal_state_sequence[k - 1],
+                nominal_control_sequence[k - 1] + δu[k - 1])
+
+            (A, B) = find_A_B(nominal_state_sequence[k],
+                nominal_control_sequence[k - 1] + δu[k - 1])
+
+            δx[k] = A * δx[k - 1] + B * δu[k - 1]
+        end
+
+        new_cost = cost_function(Q, R, Q_T, nominal_state_sequence,
+            nominal_control_sequence + δu, x_T, T)
+        iter += 1
+
+        if old_cost - new_cost < 0
+            println("oops")
+            return
+        else
+            nominal_control_sequence += δu
+        end
+    end
+
+    if norm(δu, 2) <= threshold
+        println("Converged")
+        return (nominal_state_sequence, nominal_control_sequence)
+    elseif iter >= iter_limit
+        println("Reached iter limit")
+        return (nominal_state_sequence, nominal_control_sequence)
+    else
+        println("I don't know what happened please call 911")
+        return nothing
+    end
+
+end
+
+
+export solve_ilqr, solve_ilqr_v3
